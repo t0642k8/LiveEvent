97fa9eb50f5b486769b4be9e9baf143920846d7f
James Flaherty <t0642k8@>
2020-09-03 23:59:34 -0400

    Start

---
+++ LiveEvents/Models/VideoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveEvents.Models
+{
+    public class VideoModel
+    {
+        public int MyVideoID { get; set; }
+        public string TitleVideo { get; set; }
+        public string AssetID { get; set; }
+        public string StreamuRL { get; set; }
+        public string PicURL { get; set; }
+        public string ccURL { get; set; }
+        public string OrgURL { get; set; }
+        public string Status { get; set; }
+
+        public DateTime DateLive { get; set; }
+        public string Desc { get; set; }
+    }
+}
